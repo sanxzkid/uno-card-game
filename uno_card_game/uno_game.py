e19ed23f73b9844ab55a198f4b6cd58e00fd7c79
Felipe Sanchez <f.sanxz@gmail.com>
2023-12-28 01:46:57 -0300

    split cards for each player

--- uno_card_game/uno_game.py
+++ uno_card_game/uno_game.py
@@ -1,7 +1,22 @@
+from uno_player import UnoPlayer
 from uno_deck import UnoDeck
+from termcolor import colored
 
 
 class UnoGame:
     def __init__(self):
         self.deck = UnoDeck().shuffle()
-        self.players = []
+        print("\nShuffle Deck")
+        [print(c) for c in self.deck]
+
+        self.orientation = "CW"
+
+        self.player1 = UnoPlayer("Player 1", 0, self.deck[0:8])
+        print("\Hand Player 1")
+        [print(c) for c in self.player1.hand]
+        
+        self.player2 = UnoPlayer("Player 2", 0, self.deck[8:15])
+        print("\Hand Player 2")
+        [print(c) for c in self.player2.hand]
+
+        del self.deck[0:15]
