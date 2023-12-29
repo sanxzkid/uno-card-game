0dff6d5f10488d2b53c3203427b066e7ebb1f0f7
Felipe Sanchez <f.sanxz@gmail.com>
2023-12-29 01:48:12 -0300

    add test uno_game

--- tests/test_uno_game.py
+++ tests/test_uno_game.py
@@ -1,5 +1,11 @@
+from unittest.mock import patch, MagicMock
+
 import pytest
-from unittest.mock import patch
+
+from uno_card_game.uno_card import UnoCard
+from uno_card_game.uno_deck import UnoDeck
+from uno_card_game.uno_game import UnoGame
+
 
 # Test initialization of the UnoGame
 def test_init():
@@ -13,6 +19,7 @@ def test_init():
     assert game.player_idx == 0
     assert isinstance(game.top_card, UnoCard)
 
+
 # Test the create_players method
 def test_create_players():
     game = UnoGame(hand_size=7, players_size=4)
@@ -20,6 +27,7 @@ def test_create_players():
     assert len(players) == 4
     for player in players:
         assert len(player.hand) == 7
+
 
 # Test the change_color method with mocking input
 @patch('builtins.input', side_effect=['1'])
@@ -29,6 +37,7 @@ def test_change_color(mock_input):
     game.change_color()
     assert game.top_card.color == game.uno_deck.deck_colors[0].color
 
+
 # Test the buy method
 def test_buy():
     game = UnoGame(hand_size=7, players_size=4)
@@ -37,13 +46,15 @@ def test_buy():
     assert len(game.player_hand) == 1
     assert len(game.deck) == initial_deck_size - 1
 
+
 # Test the restore_dead_deck method
 def test_restore_dead_deck():
     game = UnoGame(hand_size=7, players_size=4)
     game.dead_deck = [UnoCard('red', '1'), UnoCard('blue', '2')]
     game.restore_dead_deck()
-    assert len(game.deck) == len(game.dead_deck)
+    assert len(game.deck) == 2
     assert len(game.dead_deck) == 0
+
 
 # Test the invert method
 def test_invert():
@@ -53,6 +64,7 @@ def test_invert():
     assert game.orientation == "CCW"
     game.invert()
     assert game.orientation == "CW"
+
 
 # Test the next_player method
 def test_next_player():
@@ -69,6 +81,7 @@ def test_next_player():
     game.next_player()
     assert game.player_idx == len(game.players) - 1
 
+
 # Test the refresh_screen method with mocking os.system
 @patch('os.system', MagicMock())
 def test_refresh_screen():
@@ -77,6 +90,7 @@ def test_refresh_screen():
     # Assertions can be made about the expected state after refreshing the screen
     # However, since this method primarily prints to the console and clears the screen,
     # there may not be much to assert on without capturing stdout.
+
 
 # Test the waiting_screen method with mocking os.system and input
 @patch('os.system', MagicMock())
@@ -87,12 +101,14 @@ def test_waiting_screen():
     # Similar to refresh_screen, this method's main actions are to clear the screen and wait for input,
     # so assertions would be limited.
 
+
 # Test the show_header method
 def test_show_header(capsys):
     game = UnoGame(hand_size=7, players_size=4)
     game.show_header()
     captured = capsys.readouterr()
     assert "Uno Card Game" in captured.out
+
 
 # Test the show_error method
 def test_show_error(capsys):
@@ -103,14 +119,16 @@ def test_show_error(capsys):
     assert "Test Error" in captured.out
     assert game.error == ""
 
+
 # Test the show_cards method
 def test_show_cards(capsys):
     game = UnoGame(hand_size=7, players_size=4)
     cards = [UnoCard('red', '1'), UnoCard('blue', '2')]
     game.show_cards("Test Cards", cards)
     captured = capsys.readouterr()
-    assert "(1)red 1" in captured.out
-    assert "(2)blue 2" in captured.out
+    assert "(1)[red]" in captured.out
+    assert "(2)[blue]" in captured.out
+
 
 # Run the tests
 if __name__ == "__main__":
